bf81b3d63b489c4359d9f873bd3a58d6b9c4f622
noffle <sww@eight45.net>
2018-09-08 12:35:24 -0700

    electron app wrapper

---
+++ app.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env electron
+
+var path = require('path')
+var electron = require('electron')
+
+// GPU acceleration for my old laptop
+electron.app.commandLine.appendSwitch('ignore-gpu-blacklist', 'true')
+
+electron.app.once('ready', createWindow)
+
+function createWindow () {
+  var INDEX = 'file://' + path.resolve(__dirname, './index.html')
+  var winOpts = {
+    title: 'CHAIN SWORD',
+    width: 1024,
+    height: 768,
+    modal: true,
+    show: false,
+    // TODO: fullscreen
+    // TODO: no scroll bars
+    // TODO: no window decorations
+    alwaysOnTop: true
+  }
+  var win = new electron.BrowserWindow(winOpts)
+  win.once('ready-to-show', function () {
+    win.setMenu(null)
+    win.show()
+    win.webContents.openDevTools()
+  })
+  win.loadURL(INDEX)
+}
